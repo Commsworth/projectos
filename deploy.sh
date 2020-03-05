fe054bb09a0d65e707f8697f9791ed8eff527d20
t4christ <bakaretemitayo712@gmail.com>
2020-03-05 15:20:29 +0100

    fix merge conflicts

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ docker push commsworthdevops/projectos:$SHA
 
 # Apply kubernetes configuration
 kubectl apply -f  k8s
-kubectl set image deployments/projectos-deployment client=commsworthdevops/projectos:$SHA
+kubectl set image deployments/projectos-deployment projectos=commsworthdevops/projectos:$SHA
 
 
