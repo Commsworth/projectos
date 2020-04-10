1298c6ebe370c50a77bf50ed78ad0ae3a482201d
Nicholas Okonneh <nirvanaklaus@gmail.com>
2020-04-10 19:42:22 +0100

    Day 10 final changes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import big2 from './asset/svg/icons/big2.svg';
 import big3 from './asset/svg/icons/big3.svg';
 import big4 from './asset/svg/icons/big4.svg';
 import big5 from './asset/svg/icons/big5.svg';
+import cyber from './pages/contact/img/cyber.png';
+import cloud from './pages/contact/img/cloud.png';
+import appdev from './pages/contact/img/appdev.png';
 
 // import M from 'materialize-css';
 
@@ -30,15 +33,15 @@ function App() {
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image2="" title3="Cyber Security" content3="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image2={cyber} title3="Cyber Security" content3="We implement network security solutions designed to <br/>
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image3="" title4="App Development" content4="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image3={cloud} title4="App Development" content4="We implement network security solutions designed to <br/>
                          dynamically adapt to the evolving IT infrasture in<br/>
                          order to defend its rapidly changing attack surface.<br/>
                           Primary focus is given to the edge of the network as every<br/>
-                          packet must be certified as non-malicious before entry to the network" image4=""  {...props} />} />
+                          packet must be certified as non-malicious before entry to the network" image4={appdev}  {...props} />} />
       <Route exact path="/commsworth/explore2" render={(props) => <Contact title1="Support" content1="We are focused on developing data-oriented <br/>
                         applications tailored to solve a vast array of business <br/>
                         requiremenents while giving intuitive insights for <br/>
@@ -46,15 +49,15 @@ function App() {
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image2="" title3="Cyber Security" content3="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image2={cyber} title3="Cyber Security" content3="We implement network security solutions designed to <br/>
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image3="" title4="App Development" content4="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image3={cloud} title4="App Development" content4="We implement network security solutions designed to <br/>
                          dynamically adapt to the evolving IT infrasture in<br/>
                          order to defend its rapidly changing attack surface.<br/>
                           Primary focus is given to the edge of the network as every<br/>
-                          packet must be certified as non-malicious before entry to the network" image4=""  {...props} />} />
+                          packet must be certified as non-malicious before entry to the network" image4={appdev}  {...props} />} />
       <Route exact path="/commsworth/explore3" render={(props) => <Contact title1="Business Continuity" content1="We are focused on developing data-oriented <br/>
                         applications tailored to solve a vast array of business <br/>
                         requiremenents while giving intuitive insights for <br/>
@@ -62,15 +65,15 @@ function App() {
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image2="" title3="Cyber Security" content3="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image2={cyber} title3="Cyber Security" content3="We implement network security solutions designed to <br/>
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image3="" title4="App Development" content4="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image3={cloud} title4="App Development" content4="We implement network security solutions designed to <br/>
                          dynamically adapt to the evolving IT infrasture in<br/>
                          order to defend its rapidly changing attack surface.<br/>
                           Primary focus is given to the edge of the network as every<br/>
-                          packet must be certified as non-malicious before entry to the network" image4=""  {...props} />} />
+                          packet must be certified as non-malicious before entry to the network" image4={appdev}  {...props} />} />
       <Route exact path="/commsworth/explore4" render={(props) => <Contact title1="Product & Digital" content1="We are focused on developing data-oriented <br/>
                         applications tailored to solve a vast array of business <br/>
                         requiremenents while giving intuitive insights for <br/>
@@ -78,15 +81,15 @@ function App() {
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image2="" title3="Cyber Security" content3="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image2={cyber} title3="Cyber Security" content3="We implement network security solutions designed to <br/>
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image3="" title4="App Development" content4="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image3={cloud} title4="App Development" content4="We implement network security solutions designed to <br/>
                          dynamically adapt to the evolving IT infrasture in<br/>
                          order to defend its rapidly changing attack surface.<br/>
                           Primary focus is given to the edge of the network as every<br/>
-                          packet must be certified as non-malicious before entry to the network" image4=""  {...props} />} />
+                          packet must be certified as non-malicious before entry to the network" image4={appdev}  {...props} />} />
       <Route exact path="/commsworth/explore5" render={(props) => <Contact title1="Strategy" content1="We are focused on developing data-oriented <br/>
                         applications tailored to solve a vast array of business <br/>
                         requiremenents while giving intuitive insights for <br/>
@@ -94,15 +97,15 @@ function App() {
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image2="" title3="Cyber Security" content3="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image2={cyber} title3="Cyber Security" content3="We implement network security solutions designed to <br/>
                         dynamically adapt to the evolving IT infrasture in<br/>
                         order to defend its rapidly changing attack surface.<br/>
                          Primary focus is given to the edge of the network as every<br/>
-                         packet must be certified as non-malicious before entry to the network " image3="" title4="App Development" content4="We implement network security solutions designed to <br/>
+                         packet must be certified as non-malicious before entry to the network " image3={cloud} title4="App Development" content4="We implement network security solutions designed to <br/>
                          dynamically adapt to the evolving IT infrasture in<br/>
                          order to defend its rapidly changing attack surface.<br/>
                           Primary focus is given to the edge of the network as every<br/>
-                          packet must be certified as non-malicious before entry to the network" image4=""  {...props} />} />
+                          packet must be certified as non-malicious before entry to the network" image4={appdev}  {...props} />} />
       </Switch>
       <Footer/>
     </div>
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -25,7 +25,7 @@ class Contact extends React.Component {
     }
     render() {
         return (
-            <div>
+            <div className="explore">
                 {/* <Nav /> */}
                 <div className="tp">
                     <div className="tp1">
