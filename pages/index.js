ac23e31fad5f4011f6850b3c3b621f20db37f50d
Nicholas Okonneh <nirvanaklaus@gmail.com>
2020-05-23 10:12:18 +0100

    spacing

--- pages/index.js
+++ pages/index.js
@@ -104,7 +104,7 @@ export default class Home extends Component{
           <svg className={this.state.display ? 'div2-svg' : ''} onClick={()=>{this.setState({display:true})}} width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="6" cy="6" r="5.5" stroke="#208FFF" />
           </svg>
-
+ 
           <svg className={this.state.display ? ' ' : 'div2-svg'} onClick={() => { this.setState({ display: false }) }} width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="6" cy="6" r="5.5" stroke="#208FFF" />
           </svg>
