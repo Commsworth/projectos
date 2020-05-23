00020f8ec0b7daca4c8376e7f936f47da670f29f
abioduncomms <61694936+abioduncomms@users.noreply.github.com>
2020-05-23 17:57:56 +0200

    Update Navbar.js

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -42,7 +42,7 @@ export class Nav extends Component {
 
         <div className="nav">
           <ul className="nav-sm">
-            <li className="padding dp" > <Link href="/"><img src="/image/logo.svg" alt="Logo" /></Link>
+            <li className="padding dp" > <Link href="/"><img src="/commsworth/image/logo.svg" alt="Logo" /></Link>
               <div id="myDropdown" class="dropdown-content">
                 <ul>
                   <li><Link href="/">Academy</Link></li>
