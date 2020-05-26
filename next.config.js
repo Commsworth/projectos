221279fa896321333883efaaf9f144ec27dc1d3b
tcomms <tbakare@commsworth.com>
2020-05-26 22:08:12 +0100

    modify package.json

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  /* config options here */
-})
+// const withSass = require('@zeit/next-sass')
+// module.exports = withSass({
+//   /* config options here */
+// })
+
+module.exports = {
+  exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  }
+};
