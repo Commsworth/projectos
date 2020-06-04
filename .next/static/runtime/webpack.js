d8bcb79546ecf980decd8f9e193d74f5f8038872
Pelumi <akinola001pelumi@gmail.com>
2020-06-04 22:41:43 +0100

    chnages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3fd0bbbd4aff00b88d8c";
+/******/ 	var hotCurrentHash = "6932c8fd2ab8ed46001b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
