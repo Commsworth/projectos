c992a83fdf5e6af5716c8df5e1a20cf27fb04bb2
Nicholas Okonneh <nirvanaklaus@gmail.com>
2020-07-06 09:45:18 +0100

    06-07-2020 minor change

--- pages/offerLanding/[id]/index.js
+++ pages/offerLanding/[id]/index.js
@@ -9,13 +9,13 @@ const OfferLanding = () => {
     const router = useRouter()
     const { id } = router.query
     const data=[
-        {Header: "Digital Experience Monitoring from Dynatrace", content1: ["Optimize user experiences by ensuring every application is available, functional, fast and efficient across web, mobile, and IoT channels."] },
-        {Header: "Kubernetes Monitoring: Fact Sheet", content1: ["Kubernetes is quickly becoming the default container orchestration engine for the enterprise. Using automation, Kubernetes can manage billions of containers and scale with ease. Shouldn’t your monitoring solution be able to do the same?"] },
-        {Header: "A Guide to Cloud Native Microservices", content1: ["In this guide to cloud native microservices, industry thought leader, The New Stack, explores best practice in building, deploying and managing these scaled-out architectures.", "The guide focuses on the human element in all this. The intention is to help readers understand the complexities of shifting DevOps cultures, the people involved and the tools they employ across three key stages:", "1. What people need to know when shifting to microservices 2. Success strategies and practices when deploying microservices 3. Insights into managing microservices in production","Download your copy and get expert insight from Dynatrace DevOps activist Andreas Grabner on breaking up your monolith into microservices."] },
-        {Header: "AIOPs Done Right: eBook", content1:["As today’s enterprise systems increase in size, the benefits of digitization and cloud computing go hand in hand with technological complexity and operational risks. Download this eBook to see how AI promises to enable autonomous operations, boost innovation, and offer new modes of customer engagement by automating everything"]},
-        {Header: "Five Key Considerations for Enterprise Cloud Monitoring: eBook", content1: ["Software is taking over the world. As a result, every business needs to embrace software as a core competency to ensure survival and prosperity. However, the transformation into a software company is a significant task, as building and running software today is harder than ever. ", "This eBook outlines key considerations as you move through the journey to embrace modern software technologies, platforms and elements to truly set up to deliver your business software faster, and with higher quality, in the enterprise cloud. "]},
-        {Header: "Life made simpler with Dynatrace, through the eyes of the customer: Video", content1: ["Software is taking over the world. As a result, every business needs to embrace software as a core competency to ensure survival and prosperity. However, the transformation into a software company is a significant task, as building and running software today is harder than ever."]},
-        {Header: "", content1: ["This eBook outlines key considerations as you move through the journey to embrace modern software technologies, platforms and elements to truly set up to deliver your business software faster, and with higher quality, in the enterprise cloud. "]}
+        {Header: "Digital Experience Monitoring from Dynatrace", content1: [["Optimize user experiences by ensuring every application is available, functional, fast and efficient across web, mobile, and IoT channels.", "text"]] },
+        {Header: "Kubernetes Monitoring: Fact Sheet", content1: [["Kubernetes is quickly becoming the default container orchestration engine for the enterprise. Using automation, Kubernetes can manage billions of containers and scale with ease. Shouldn’t your monitoring solution be able to do the same?", "text"]] },
+        {Header: "A Guide to Cloud Native Microservices", content1: [["In this guide to cloud native microservices, industry thought leader, The New Stack, explores best practice in building, deploying and managing these scaled-out architectures.", "text"], ["The guide focuses on the human element in all this. The intention is to help readers understand the complexities of shifting DevOps cultures, the people involved and the tools they employ across three key stages:", "text"],[["1. What people need to know when shifting to microservices", "2. Success strategies and practices when deploying microservices", "3. Insights into managing microservices in production"], "list"],["Download your copy and get expert insight from Dynatrace DevOps activist Andreas Grabner on breaking up your monolith into microservices.", "text"]] },
+        {Header: "AIOPs Done Right: eBook", content1:[["As today’s enterprise systems increase in size, the benefits of digitization and cloud computing go hand in hand with technological complexity and operational risks. Download this eBook to see how AI promises to enable autonomous operations, boost innovation, and offer new modes of customer engagement by automating everything", "text"]]},
+        {Header: "Five Key Considerations for Enterprise Cloud Monitoring: eBook", content1: [["Software is taking over the world. As a result, every business needs to embrace software as a core competency to ensure survival and prosperity. However, the transformation into a software company is a significant task, as building and running software today is harder than ever. ", "text"], ["This eBook outlines key considerations as you move through the journey to embrace modern software technologies, platforms and elements to truly set up to deliver your business software faster, and with higher quality, in the enterprise cloud. ", "text"]]},
+        {Header: "Life made simpler with Dynatrace, through the eyes of the customer: Video", content1: [["Software is taking over the world. As a result, every business needs to embrace software as a core competency to ensure survival and prosperity. However, the transformation into a software company is a significant task, as building and running software today is harder than ever.","text"]]},
+        {Header: "", content1: [["This eBook outlines key considerations as you move through the journey to embrace modern software technologies, platforms and elements to truly set up to deliver your business software faster, and with higher quality, in the enterprise cloud. ", "text"]]}
     ]
     let [dataID, setDataID] = useState(data.length-1);
     useEffect(
@@ -25,7 +25,8 @@ const OfferLanding = () => {
     } ,[]
     )
     return (
-        <Layout>
+        // <Layout>
+        <>
              <Head>
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -56,7 +57,18 @@ const OfferLanding = () => {
                         <path d="M2 2H30" stroke="#076fd9" strokeWidth="4" strokeLinecap="round" />
                     </svg>
                     </div>
-                    {data[dataID].content1.map(cont=><p>{cont}</p>)}
+                    {data[dataID].content1.map(cont=>{
+                    switch (cont[cont.length-1]) {
+                        case "text":
+                            return <p style={{margin: "20px 0px"}}>{cont[0]}</p>
+                            // break;
+                        case "list":
+                            return cont[0].map(con => <p style={{margin: "10px 30px"}}>{con}</p>)
+                    
+                        default:
+                            break;
+                    }
+                    })}
                     {/* <p>The Cyber Threat Assessment Program (CTAP) is 
                         designed to help you during greenfield and renewal 
                         opportunities to convert prospects and expand your 
@@ -85,7 +97,8 @@ const OfferLanding = () => {
                     <Socials />
             
               </main>
-        </Layout>
+              </>
+        // </Layout>
     );
 };
 
