5f36c965a59a7f21c0a15540357d5eb2bc8abb68
Nicholas Okonneh <nirvanaklaus@gmail.com>
2020-07-06 16:04:13 +0100

    ....

--- pages/ctap.js
+++ pages/ctap.js
@@ -36,7 +36,7 @@ this.state={
                 <div className="ct">
                     <div className="ctap-head">
                         <div className="overlay">
-                            <div className='container-landing'>
+                            <div className='container'>
                                 <h1>Cyber Threat Assessment Program</h1>
                             </div>
                         </div>
