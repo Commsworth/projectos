0aa1906032560345e1c8d08472db40d7d9e6a987
Pelumi <akinola001pelumi@gmail.com>
2020-07-28 10:38:27 +0100

    contact page

---
+++ components/extraPageComponents/Socials.jsx
@@ -0,0 +1,128 @@
+const Socials = ({icons}) => {
+    return (
+        icons==="white"?
+        <div className="sc">
+          <div className="socials">
+        <div className="follow">
+          <p>Follow us:</p>
+          <img src="/whitetwitter.svg" alt="twitter"/>
+          <img src="/whitefacebook.svg" alt="facebook"/>
+          <img src="/whitelinkedin.svg" alt="linkedin"/>
+        </div>
+        <div className="share">
+          <p>Share on:</p>
+          <img src="/whitetwitter.svg" alt="twitter"/>
+          <img src="/whitefacebook.svg" alt="facebook"/>
+          <img src="/whitelinkedin.svg" alt="linkedin"/>
+        </div>
+      </div>
+      </div>
+        :
+        <div className="sc">
+          <div className="socials">
+        <div className="follow">
+          <p>Follow us:</p>
+          <img src="/twitter.svg" alt="twitter"/>
+          <img src="/facebook.svg" alt="facebook"/>
+          <img src="/linkedin.svg" alt="linkedin"/>
+        </div>
+        <div className="share">
+          <p>Share on:</p>
+          <img src="/twitter.svg" alt="twitter"/>
+          <img src="/facebook.svg" alt="facebook"/>
+          <img src="/linkedin.svg" alt="linkedin"/>
+        </div>
+      </div>
+      <style jsx>{`
+          .sc {
+  border-top: 1px solid  #C1C1C1;
+  border-bottom: 1px solid  #C1C1C1;
+  background: white;
+}
+
+.sc .socials {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: justify;
+      -ms-flex-pack: justify;
+          justify-content: space-between;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+  width: 90vw;
+  margin: 2vw 5vw;
+  margin-top: 8rem;
+  padding: 5px;
+  margin: auto;
+}
+
+.sc .socials .follow {
+  width: 15rem;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: space-evenly;
+      -ms-flex-pack: space-evenly;
+          justify-content: space-evenly;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+}
+
+.sc .socials .follow p {
+  font-size: 16px;
+  line-height: 119%;
+  /* identical to box height, or 19px */
+  /* Text color */
+  color: #1B1B1B;
+}
+
+.sc .socials .follow img {
+  cursor: pointer;
+}
+
+.sc .socials .share {
+  width: 15rem;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: space-evenly;
+      -ms-flex-pack: space-evenly;
+          justify-content: space-evenly;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+}
+
+.sc .socials .share p {
+  font-size: 16px;
+  line-height: 119%;
+  /* identical to box height, or 19px */
+  /* Text color */
+  color: #1B1B1B;
+}
+
+.sc .socials .share img {
+  cursor: pointer;
+}
+
+@media only screen and (max-width: 570px) {
+  .sc .socials {
+    /* width: 15rem; */
+    display: -ms-grid;
+    display: grid;
+    /* justify-content: space-evenly; */
+    /* align-items: center; */
+  }
+  .sc .socials .follow,
+  .sc .socials .share {
+    margin: 0vh 5vw;
+  }
+}
+          `}</style>
+      </div>
+)
+};
+
+export default Socials;
---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+// import Nav from '../components/navbar/Navbar';
+// import Footer from '../components/footer/Footer';
+import '../public/css/global.css'
+
+export default function MyApp({ Component, pageProps }) {
+    return(
+      <div className="main-container">
+        {/* <Nav/> */}
+        <Component {...pageProps} />
+        {/* <Footer/> */}
+      </div>
+      )
+  }
