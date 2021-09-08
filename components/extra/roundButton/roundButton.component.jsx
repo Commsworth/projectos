36a475e84dbfc099772b13cded97799a82f02b8b
Nicklaus <nirvanaklaus@gmail.com>
2021-09-08 11:22:08 +0100

    deletions

--- components/extra/roundButton/roundButton.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './roundButton.styles.scss';
-
-const RoundButton = (props) => (
-    <button className="round-button">{props.display}</button>
-)
-
-export default RoundButton;
