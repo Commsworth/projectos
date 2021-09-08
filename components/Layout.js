2e04a2a3b775932102ad7e7a35d89baa3697e3ce
Nicklaus <nirvanaklaus@gmail.com>
2021-09-08 11:30:27 +0100

    deletions

---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+// import Head from 'next/head';
+// import Navbar from './Navbar';
+// import Footer from './footer/Footer'
+
+
+// const Layout = (props) => (
+//   <React.Fragment>
+   
+//     <Head>
+//       <title>Commsworth</title>
+//       <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet"></link>
+//     </Head>
+//     <Navbar />
+
+//     {props.children}
+    
+//     {/* <Footer /> */}
+
+//     <style jsx>
+//         {`
+//         h1,h2,h3,h4,h5,h6{
+//             font-family: 'Nunito', sans-serif;
+//         }
+//         `}
+//     </style>
+   
+//   </React.Fragment>
+// );
+
+// export default Layout;
